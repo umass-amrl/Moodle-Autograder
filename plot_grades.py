3710b2126ca61857d66c49f604930dec171dbd31
Kyle Vedder <kyle.c.vedder@gmail.com>
2017-11-08 23:10:44 -0500

    Added bounds to the sectioning to make it clearer what is inclusive or not.

--- plot_grades.py
+++ plot_grades.py
@@ -13,10 +13,10 @@ grades = [row[4] for row in reader]
 grades = sorted([float(g) for g in grades[1:]])
 print("Grades: {}".format(grades))
 print("Students 100+: {}".format(len([g for g in grades if g >= 100])))
-print("Students 90 to 100: {}".format(len([g for g in grades if g >= 90 and g < 100])))
-print("Students 80 to 90: {}".format(len([g for g in grades if g >= 80 and g < 90])))
-print("Students 70 to 80: {}".format(len([g for g in grades if g >= 70 and g < 80])))
-print("Students 60 to 70: {}".format(len([g for g in grades if g >= 60 and g < 70])))
+print("Students [90 to 100): {}".format(len([g for g in grades if g >= 90 and g < 100])))
+print("Students [80 to 90): {}".format(len([g for g in grades if g >= 80 and g < 90])))
+print("Students [70 to 80): {}".format(len([g for g in grades if g >= 70 and g < 80])))
+print("Students [60 to 70): {}".format(len([g for g in grades if g >= 60 and g < 70])))
 print("Students below 60: {}".format(len([g for g in grades if g < 60])))
 bin_size = 1
 max_grade = 110
