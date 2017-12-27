af84293d23f0b4e6da00230073fde5599216e763
Khoshrav Doctor <khoshrav@gmail.com>
2017-12-27 11:11:53 -0500

    Updated late day calculator

--- late-calculate.py
+++ late-calculate.py
@@ -34,7 +34,7 @@ if len(sys.argv) < 3:
 output_worksheet = sys.argv[1]
 grading_worksheets = sys.argv[2:]
 print ('Output worksheet : "' + output_worksheet + '"')
-
+files = {}
 for assignment in grading_worksheets:
   if not os.path.isfile(assignment):
     print ('ERROR: {} is not a valid file!'.format(assignment))
@@ -45,72 +45,80 @@ def get_fieldnames(file):
                         delimiter=',', quotechar='"').fieldnames
 
 def setup_output_writer(output_worksheet, grading_worksheets):
+  header_list = ['Email address']#get_fieldnames(grading_worksheets[0])
+  for grading_worksheet in grading_worksheets:
+    header_list.append(grading_worksheet + "_Late_Days")
+    header_list.append(grading_worksheet + "_Grade")
+  print(header_list)
+  header_list.append('Total Late Days')
   writer = csv.DictWriter(open(output_worksheet, 'wt'),
                         delimiter = ',',
                         quotechar = '"',
-                        fieldnames = get_fieldnames(grading_worksheets[0]))
+                        fieldnames = header_list)
   writer.writeheader()
   return writer
 
 def setup_input_readers(grading_worksheets):
-  return [csv.DictReader(open(grading_worksheet, 'rt'), delimiter=',',
-                         quotechar='"') for grading_worksheet in grading_worksheets]
+  readers = {}
+  for grading_worksheet in grading_worksheets:
+    files[grading_worksheet] = open(grading_worksheet, 'rt')
+    readers[grading_worksheet] = csv.DictReader(files[grading_worksheet],
+      delimiter=',',quotechar='"')
+  return readers
 
-def get_late_days(reader):
-  raw_statuses = [row['Status'] for row in reader]
-  def parse_status(status):
-    if "No submission" in status:
-      return ("No submission.", 0)
-    elif "late" in status:
-      late_string = status.split(" - ")[1]
-      late_list = late_string.split(' ')[:-1]
-      days_used = 0.0
-      for i in range(int(len(late_list) / 2)):
-        val = int(late_list[2 * i])
-        modifier = late_list[2 * i + 1]
-        if "day" in modifier:
-          days_used += val
-        elif "hour" in modifier:
-          days_used += (val / 24)
-        elif "mins" in modifier:
-          days_used += (val / 24 / 60)
-        elif "sec" in modifier:
-          days_used += (val / 24 / 60 / 60)
-      leniency_amount = 20 / 60 / 24
-      return (("Handed in " + late_string + "!"), int(math.ceil(days_used - leniency_amount)))
-    else:
-      return ("Handed in on time!", 0)
-  return [parse_status(status) for status in raw_statuses]
+def parse_status(status):
+  if "No submission" in status:
+    return 0
+  elif "late" in status:
+    late_string = status.split(" - ")[1]
+    late_list = late_string.split(' ')[:-1]
+    days_used = 0.0
+    for i in range(int(len(late_list) / 2)):
+      val = int(late_list[2 * i])
+      modifier = late_list[2 * i + 1]
+      if "day" in modifier:
+        days_used += val
+      elif "hour" in modifier:
+        days_used += (val / 24)
+      elif "mins" in modifier:
+        days_used += (val / 24 / 60)
+      elif "sec" in modifier:
+        days_used += (val / 24 / 60 / 60)
+    leniency_amount = 20 / 24 / 60
+    return int(math.ceil(days_used - leniency_amount))
+  else:
+    return 0
 
-def merge_late_days(list_of_assignment_late_days):
-  base_list = [[assignment] for assignment in list_of_assignment_late_days[0]]
-  for assignment in list_of_assignment_late_days[1:]:
-    for idx, grade in enumerate(assignment):
-      base_list[idx].append(grade)
-  return base_list
-
-def calculate_late_sum(merged_student):
-  # reduce(lambda total_days, assignment: total_days + assignment[1], merged_student, 0)
-  days_used = 0
-  days_used_list = []
-  for assignment in merged_student:
-    days_used += assignment[1]
-    days_used_list.append(days_used)
-  return [(assignment, total) for assignment, total in zip(merged_student, days_used_list)]
-
-def generate_final_string(student):
-  string_list = ["{} Late days used on this assignment: {}. Total late days used: {}\n".format(assignment[0][0], assignment[0][1], assignment[1]) for assignment in student]
-  return reduce(lambda acc, s: acc + s, string_list, "")
+def get_student_score(readers, student_email):
+  student_late_days = {}
+  student_grades = {}
+  for grading_worksheet in grading_worksheets:
+    student_late_days[grading_worksheet+"_Late_Days"] = ""
+    for row in readers[grading_worksheet]:
+      if row['Email address'] == student_email:
+        student_late_days[grading_worksheet+"_Late_Days"] = parse_status(row['Status'])
+        student_grades[grading_worksheet + "_Grade"] = row['Grade']
+        break
+    files[grading_worksheet].seek(0)
+  return student_late_days, student_grades
 
 writer = setup_output_writer(output_worksheet, grading_worksheets)
 readers = setup_input_readers(grading_worksheets)
-result_csv_format = setup_input_readers([grading_worksheets[0]])[0]
+late_days_used = {}
+student_grades = {}
+reference_reader = csv.DictReader(open(grading_worksheets[0],
+                                  'rt'),delimiter=',',quotechar='"') 
+for row in reference_reader:
+  late_days_used[row['Email address']] , \
+  student_grades[row['Email address']]= get_student_score(readers, row['Email address'])
 
-late_days = [get_late_days(reader) for reader in readers]
-late_days_with_sum = [calculate_late_sum(student_late_days) for student_late_days in merge_late_days(late_days)]
-final_strings = [generate_final_string(student) for student in late_days_with_sum]
-
-for row, final_string in zip(result_csv_format, final_strings):
-  row['Grade'] = 100
-  row['Feedback comments'] = final_string
+for student_email, student_late_days in late_days_used.items():
+  row = {}
+  row['Total Late Days'] = 0
+  row['Email address'] = student_email
+  for assignment, grade in student_grades[student_email].items(): 
+    row[assignment] = grade
+  for assignment, late_day in student_late_days.items():
+    row[assignment] = late_day
+    row['Total Late Days'] = row['Total Late Days'] + late_day
   writer.writerow(row)
